094da65d10de113dd7618dd820c5af8b61387a5a
Praise Udeh <mailpraiseudeh@gmail.com>
2021-08-24 21:50:51 +0100

    commit message

--- django_project/settings.py
+++ django_project/settings.py
@@ -144,7 +144,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE= 'blog.storage.WhiteNoiseStaticFilesStorage'
-STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE= 'django_project.storage.WhiteNoiseStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
